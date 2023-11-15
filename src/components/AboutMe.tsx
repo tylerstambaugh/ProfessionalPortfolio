5a56133ebf2e731d760a04250ed8bfe5dd374f7c
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-11-15 08:41:52 -0500

    update about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,7 +3,7 @@ import Headshot from "../assets/images/headshot_sized.png";
 import { Col, Container, Row, Image } from "react-bootstrap";
 
 const greeting = "Howdy, I am Tyler!";
-const bio1 = `Indiana native with an early tech spark – my journey kicked off with a garage sale gem, a Commodore 64, sparking a lifelong love for technology. 
+const bio1 = `Indiana native with a passion for technology and learning. My journey kicked off with a garage sale gem, a Commodore 64, sparking a lifelong interest in computers. 
    While at Indiana University, I earned a B.S. in Informatics, delving into the tech landscape. I began my professional career in tech support, working with 
    client-server applications and databases. Over time, I progressed to become a Software Engineer, specializing in .Net and React projects. To further my skills, 
    I completed over 600 hours of coursework at Eleven Fifty Academy. In my free time, I'm working on a full-stack application.  Eager to explore process automation and enhance my software engineering prowess,
